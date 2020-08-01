f421afa8bf602a0f04b4de77656698854ff9de75
Olena Pyvnytska <lpyvnytska@gmail.com>
2020-08-01 17:33:57 +0300

    logging with morgan

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
 
 app.use(express.json())
+morgan.token('body', function (req) { 
+  return  JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const persons = [
     {
