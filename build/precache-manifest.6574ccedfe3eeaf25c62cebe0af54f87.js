370eb35d911735bbc6e1488ed3a618741a20f30c
Olena Pyvnytska <lpyvnytska@gmail.com>
2020-08-01 19:10:15 +0300

    add frontend to heroku

---
+++ build/precache-manifest.6574ccedfe3eeaf25c62cebe0af54f87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6cc83a38469ab03d88d2d37eb4d31b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba31b760c39d4ad18aad",
+    "url": "/static/css/main.6168571a.chunk.css"
+  },
+  {
+    "revision": "571f1405cf6fe3ef0364",
+    "url": "/static/js/2.7dd20ff8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7dd20ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba31b760c39d4ad18aad",
+    "url": "/static/js/main.52bfaf28.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
